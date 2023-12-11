2624f27ff9fd48b04dc7b8e9051d5376476494df
SnowyTheBear67 <lance.boza@gmail.com>
2023-12-11 15:55:14 -0500

    created Entity relationship

--- Instructor-Application/src/main/java/com/cognixia/jump/instructor/Instructor.java
+++ Instructor-Application/src/main/java/com/cognixia/jump/instructor/Instructor.java
@@ -1,7 +1,12 @@
 package com.cognixia.jump.instructor;
 
 import java.io.Serializable;
+import java.util.List;
 
+import com.cognixia.jump.student.Student;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -9,6 +14,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
@@ -46,6 +52,10 @@ public class Instructor implements Serializable {
 	@Enumerated(EnumType.STRING)
 	@Column(nullable = false)
 	private Role role;
+	
+	@OneToMany(mappedBy = "instructor", cascade = CascadeType.ALL)
+	@JsonIgnore
+	private List<Student> students;
 
 	public Instructor() {
 		
--- Instructor-Application/src/main/java/com/cognixia/jump/student/Student.java
+++ Instructor-Application/src/main/java/com/cognixia/jump/student/Student.java
@@ -3,11 +3,16 @@ package com.cognixia.jump.student;
 
 import java.io.Serializable;
 
+import com.cognixia.jump.instructor.Instructor;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
@@ -43,6 +48,10 @@ public class Student implements Serializable {
 	
 	@Column
 	private String notes;
+	
+	@ManyToOne
+	@JoinColumn(name = "instructor_id", referencedColumnName = "id")
+	private Instructor instructor;
 
 	public Student() {
 		
