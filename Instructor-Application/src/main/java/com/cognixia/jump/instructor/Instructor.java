b73ae164acc50b81d135701a352a70691c5294e1
SnowyTheBear67 <lance.boza@gmail.com>
2023-12-11 15:42:01 -0500

    initial file structure for spring boot

---
+++ Instructor-Application/src/main/java/com/cognixia/jump/instructor/Instructor.java
@@ -0,0 +1,113 @@
+package com.cognixia.jump.instructor;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Instructor implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	public static enum Role {
+		ROLE_USER, ROLE_ADMIN // roles need to start with ROLE_ & be all caps b/c security will look for this naming structure
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column
+	@NotBlank
+	private String firstName;
+	
+	@Column
+	@NotBlank
+	private String lastName;
+	
+	@Column(unique = true, nullable = false)
+	@NotBlank
+	private String username;
+	
+	@Column(nullable = false)
+	@NotBlank
+	private String password;
+	
+	@Column(columnDefinition = "boolean default true")
+	private boolean enabled;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Role role;
+
+	public Instructor() {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	
+	
+	
+	
+}
---
+++ Instructor-Application/src/main/java/com/cognixia/jump/student/Student.java
@@ -0,0 +1,118 @@
+package com.cognixia.jump.student;
+
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Student implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column
+	@NotBlank
+	private String firstName;
+	
+	@Column
+	@NotBlank
+	private String lastName;
+	
+	@Column
+	@NotBlank
+	private String email;
+	
+	@Min(0)
+	@Max(100)
+	@Column
+	private Integer grade;
+	
+	@Column
+	private String jumpClass;
+	
+	@Column
+	private String notes;
+
+	public Student() {
+		
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getGrade() {
+		return grade;
+	}
+
+	public void setGrade(Integer grade) {
+		this.grade = grade;
+	}
+
+	
+
+	public String getJumpClass() {
+		return jumpClass;
+	}
+
+	public void setJumpClass(String jumpClass) {
+		this.jumpClass = jumpClass;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", grade=" + grade + ", jumpClass=" + jumpClass + ", notes=" + notes + "]";
+	}
+	
+	
+	
+	
+}
